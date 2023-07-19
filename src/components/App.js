66fcef4dccf2f688c7d53d5180610731e65ce8d8
sajid <mohdsajid8010@gmail.com>
2023-07-19 11:34:47 +0530

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+  const [search, setSearch] = useState("");
+  const [Fruits, setFruits] = useState(fruits);
+
+  useEffect(() => {
+    const remainFruits = fruits.filter((value) => {
+      return value.includes(search);
+    });
+    setFruits(remainFruits);
+  }, [search]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input
+        type="text"
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      />
+      <div>
+        <ul>
+          {Fruits.length > 0 &&
+            Fruits.map((elm, i) => {
+              return <li key={i}>{elm}</li>;
+            })}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
